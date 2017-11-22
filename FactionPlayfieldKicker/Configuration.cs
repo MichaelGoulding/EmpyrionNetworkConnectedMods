003570f26619a18457e4b86f5a4f93400187c988
Michael Goulding <michael.goulding@live.com>
2017-11-21 19:46:54 -0800

    Use shared code module and add support for letting admins visit restricted playfields.

--- FactionPlayfieldKicker/Configuration.cs
+++ FactionPlayfieldKicker/Configuration.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using YamlDotNet.Serialization;
+﻿using System.Collections.Generic;
 
 namespace FactionPlayfieldKicker
 {
-    public class Configuration
+    public class Configuration : SharedCode.BaseConfiguration
     {
-        public string GameServerIp { get; set; }
-        public int GameServerApiPort { get; set; }
-
-        public int PlayerUpdateIntervalInSeconds { get; set; }
-
         public string BootMessage { get; set; }
 
         public Dictionary<string, int> FactionHomeWorlds { get; set; }
@@ -19,19 +11,7 @@ namespace FactionPlayfieldKicker
         public Configuration()
         {
             // set defaults
-            GameServerIp = "127.0.0.1";
-            GameServerApiPort = 12345;
-            PlayerUpdateIntervalInSeconds = 5;
             BootMessage = "You are not allowed to enter this faction's playfield.";
-        }
-
-        public static Configuration GetConfiguration(String filePath)
-        {
-            using (var input = File.OpenText(filePath) )
-            {
-                var deserializer = new Deserializer();
-                return deserializer.Deserialize<Configuration>(input);
-            }
         }
     }
 }
