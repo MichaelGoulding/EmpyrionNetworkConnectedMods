c91ced08fed415cb932395094d5bca3e653a1d39
Michael Goulding <michael.goulding@live.com>
2017-12-01 22:52:53 -0800

    Added comments to SellToServerMod.cs to help explain how things work.

--- SellToServerMod/SellToServerMod.cs
+++ SellToServerMod/SellToServerMod.cs
@@ -4,26 +4,42 @@ using System.Threading.Tasks;
 
 namespace SellToServerMod
 {
+    // This attribute lets the mod runner find it later.
     [System.ComponentModel.Composition.Export(typeof(IGameMod))]
     public class SellToServerMod : IGameMod
     {
+        // This is the string that will be listed when a user types "!MODS".
+        // The helper method here uses the AssemblyTitle attribute found in the AssemblyInfo.cs.
         static readonly string k_versionString = SharedCode.Helpers.GetVersionString(typeof(SellToServerMod));
 
+
+        // This is called by the mod runner before connecting to the game server during startup.
         public void Start(IGameServerConnection gameServerConnection)
         {
+            // figure out the path to the setting file in the same folder where this DLL is located.
             var configFilePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\" + "SellToServerMod_Settings.yaml";
 
+            // save connection to game server for later use
             _gameServerConnection = gameServerConnection;
+
+            // This deserializes the yaml config file
             _config = SharedCode.BaseConfiguration.GetConfiguration<Configuration>(configFilePath);
 
+            // Tell the string to use for "!MODS" command.
             _gameServerConnection.AddVersionString(k_versionString);
+
+            // Subscribe for the chat event
             _gameServerConnection.Event_ChatMessage += OnEvent_ChatMessage;
         }
 
+
+        // This is called right before the program ends.  Mods should save anything they need here.
         public void Stop()
         {
         }
 
+
+        // Event handler for when chat message are received from players.
         private void OnEvent_ChatMessage(string msg, Player player)
         {
             switch (msg)
@@ -44,8 +60,11 @@ namespace SellToServerMod
                 if (boundingBox.IsInside(player))
                 {
                     found = true;
+
+                    // This returns a task that, when completed, has the selection from the player.
                     var task = player.DoItemExchange("Sell Items - Step 1", "Place Items to get a price", "Process"); // BUG: button text can only be set once "Get Price");
 
+                    // This continues the operation later when the server returns the response.
                     task.ContinueWith(
                         async (Task<Eleon.Modding.ItemExchangeInfo> itemExchangeInfoInTask) =>
                         {
@@ -69,6 +88,7 @@ namespace SellToServerMod
 
                                 var message = string.Format("We will pay you {0} credits.", credits);
 
+                                // Here, instead of using ContinueWith, we just wait here for the response. Execution will resume as soon as we get back an answer.
                                 var itemExchangeInfoSold = await player.DoItemExchange(
                                     "Sell Items - Step 2",
                                     message,
@@ -84,12 +104,14 @@ namespace SellToServerMod
                                 }
                                 else
                                 {
-                                    // try again
+                                    // try again if the user changed things.
                                     _gameServerConnection.DebugOutput("Player {0} changed things.", player);
                                     itemExchangeInfoInQuote = itemExchangeInfoSold;
                                 }
                             }
                         });
+
+                    break;
                 }
             }
 
