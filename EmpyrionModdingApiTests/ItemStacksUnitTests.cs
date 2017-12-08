5f752f3caea1efbf0397609e3983acd845d5b04c
Michael Goulding <michael.goulding@live.com>
2017-12-08 14:13:34 -0800

    Add failing test for ItemStacks bug

---
+++ EmpyrionModdingApiTests/ItemStacksUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmpyrionModdingApiTests
+{
+    [TestClass]
+    public class ItemStacksUnitTests
+    {
+        [TestMethod]
+        public void TestAddStack()
+        {
+            var testee = new SharedCode.ItemStacks();
+
+            // test overflow on adding stacks
+            testee.AddStack(new SharedCode.ItemStack() { Id = 1, Amount = (int.MaxValue - 10) });
+            testee.AddStack(new SharedCode.ItemStack() { Id = 1, Amount = (20) });
+
+            Assert.AreEqual(2, testee.Count);
+            Assert.AreEqual(int.MaxValue, testee[0].Amount);
+            Assert.AreEqual(10, testee[1].Amount);
+        }
+    }
+}
---
+++ EmpyrionModdingApiTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpyrionModdingApiTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmpyrionModdingApiTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec5a5011-2bb6-4914-a090-d6ee42dea9c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
