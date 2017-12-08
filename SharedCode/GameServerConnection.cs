705318e06b680e5f9cb638c58ab98f626b48abbe
Michael Goulding <michael.goulding@live.com>
2017-12-08 14:53:32 -0800

    Extract RequestTracker out of GameServerConnection

--- SharedCode/GameServerConnection.cs
+++ SharedCode/GameServerConnection.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 using Eleon.Modding;
-using EPMConnector;
 
 namespace SharedCode
 {
@@ -31,52 +28,6 @@ namespace SharedCode
         List<BoundingBox> _bboxes = new List<BoundingBox>();
 
         BaseConfiguration _config;
-
-        class RequestTracker
-        {
-            private ushort _nextAvailableId = 12340;
-            private Dictionary<ushort, object/*TaskCompletionSource<T>*/> _taskCompletionSourcesById = new Dictionary<ushort, object>();
-
-            internal (ushort, Task<T>) GetNewTaskCompletionSource<T>()
-            {
-                if (_nextAvailableId == ushort.MaxValue)
-                {
-                    _nextAvailableId = 12340;
-                }
-
-                var newId = _nextAvailableId++;
-
-                var taskCompletionSource = new TaskCompletionSource<T>();
-                _taskCompletionSourcesById[newId] = taskCompletionSource;
-
-                return (newId, taskCompletionSource.Task);
-            }
-
-            internal bool TryHandleEvent(ModProtocol.Package p)
-            {
-                bool trackingIdFound = _taskCompletionSourcesById.ContainsKey(p.seqNr);
-
-                if (trackingIdFound)
-                {
-                    object taskCompletionSource = _taskCompletionSourcesById[p.seqNr];
-                    _taskCompletionSourcesById.Remove(p.seqNr);
-
-                    if (p.cmd == Eleon.Modding.CmdId.Event_Error)
-                    {
-                        Eleon.Modding.ErrorInfo eInfo = (Eleon.Modding.ErrorInfo)p.data;
-                        System.Reflection.MethodInfo setException = taskCompletionSource.GetType().GetMethod("SetException");
-                        setException.Invoke(taskCompletionSource, new[] { new Exception(eInfo.ToString()) });
-                    }
-                    else
-                    {
-                        System.Reflection.MethodInfo setResult = taskCompletionSource.GetType().GetMethod("SetResult");
-                        setResult.Invoke(taskCompletionSource, new[] { p.data });
-                    }
-                }
-
-                return trackingIdFound;
-            }
-        }
 
         private RequestTracker _requestTracker = new RequestTracker();
 
@@ -791,8 +742,6 @@ namespace SharedCode
                     }
                 }
             }
-
-
 
             if (!_playerUpdateTimer.Enabled)
             {
