64b3e60fefa3921dfd66f766fcc42f0e550665b5
Michael Goulding <michael.goulding@live.com>
2017-12-22 17:15:21 -0800

    Update version number

--- PlayfieldStructureRegenMod/Properties/AssemblyInfo.cs
+++ PlayfieldStructureRegenMod/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PlayfieldStructureRegenMod 0.1 by Mortlath")]
+[assembly: AssemblyTitle("PlayfieldStructureRegenMod 0.5 by Mortlath")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
