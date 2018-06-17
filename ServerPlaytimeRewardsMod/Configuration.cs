48146a1eb762987cec33d51d19dd04597b7f8472
Michael Goulding <michael.goulding@live.com>
2018-06-16 20:09:33 -0700

    Added ServerPlaytimeRewardsMod.

---
+++ ServerPlaytimeRewardsMod/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using EmpyrionModApi;
+using System.Collections.Generic;
+
+namespace ServerPlaytimeRewardsMod
+{
+    public class Configuration
+    {
+        public double XpRewardPeriodInMinutes { get; set; }
+
+        public int XpPerPeriod { get; set; }
+
+        public Configuration()
+        {
+            XpRewardPeriodInMinutes = 1.0;
+            XpPerPeriod = 100;
+        }
+    }
+}
